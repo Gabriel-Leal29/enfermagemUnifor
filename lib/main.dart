7d88e7be1180a59aa70f89725c44a82fe29d82ab
Gabriel Leal <gabriellealdeoliveira2005@gmail.com>
2026-03-17 01:15:33 -0300

    adicioando lib para gerenciar tamanho da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_enfermagem_desktop/pages/home.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'database/db_helper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inicializa SQLite para desktop
+  // inicializa SQLite para desktop
   sqfliteFfiInit();
   databaseFactory = databaseFactoryFfi;
 
   // inicializa o banco
   await DbHelper.instance.database;
+
+  await windowManager.ensureInitialized();
+
+  // definindo  tamanho mínimo da tela
+  WindowOptions windowOptions = const WindowOptions(
+    size: Size(1200, 800),
+    minimumSize: Size(1100, 700), // TODO: tamanho mínimo da tela, pode variar com o decorrer do projeto
+    center: true,
+    title: "Enfermagem - UNIFOR",
+  );
+
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
 
   runApp(const MyApp());
 }
